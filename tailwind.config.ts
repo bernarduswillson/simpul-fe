78111ecd3d8cc425e43d487b2da512c333347538
bewe <93899302+bernarduswillson@users.noreply.github.com>
2024-08-23 19:03:49 +0700

    feat: add colors and global.css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,36 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: {
+          blue: "#2F80ED",
+          black: "#4F4F4F",
+          gray: '#828282',
+          white: '#E0E0E0'
+        },
+        indicator: {
+          orange: '#F8B76B',
+          purple: '#8785FF',
+          red: '#EB5757',
+          yellow: '#F2C94C',
+        },
+        chats: {
+          orange: '#E5A443',
+          'orange-pastel': '#FCEED3',
+          purple: '#9B51E0',
+          'purple-pastel': '#EEDCFF',
+          green: '#43B78D',
+          'green-pastel': '#D2F2EA'
+        },
+        stickers: {
+          100: '#E9F3FF',
+          200: '#FDCFA4',
+          300: '#F9E9C3',
+          400: '#AFEBDB',
+          500: '#CBF1C2',
+          600: '#CFCEF9',
+          700: '#F9E0FD',
+        }
       },
     },
   },
