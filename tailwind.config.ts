bfb01f52cbde1aa8f3d1b822a0df3a98b27f6b20
bewe <93899302+bernarduswillson@users.noreply.github.com>
2024-08-25 19:45:33 +0700

    fix: deployment css bug

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,12 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/api/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}", 
+    "./src/appPages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/interface/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/redux/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
